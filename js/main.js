5b6e1c081c6b6db7e013abbd35e193d3225d0f82
nikolay <koliasinitsa317@gmail.com>
2024-06-19 20:40:20 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const btnDarkMode = document.querySelector('.dark-mode-btn');
+
+
+// 1. Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+}
+
+
+// проверка темы в локал хранилище
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add('dark-mode-btn--active')
+    document.body.classList.add('dark')
+}
+
+// проверка темы на уровне системы настроек
+// window
+//     .matchMedia("(prefers-color-scheme: dark)")
+//     .addEventListener("change", (event) => {
+//         const newColorScheme = event.matches ? "dark" : "light";
+
+//         if (newColorScheme === "dark") {
+//             btnDarkMode.classList.add("dark-mode-btn--active");
+//             document.body.classList.add("dark");
+//             localStorage.setItem("darkMode", "dark");
+//         } else {
+//             btnDarkMode.classList.remove("dark-mode-btn--active");
+//             document.body.classList.remove("dark");
+//             localStorage.setItem("darkMode", "light");
+//         }
+//     });
+
+// включение дарк мода по кнопке
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle('dark-mode-btn--active');
+    const isDark = document.body.classList.toggle('dark');
+
+    if (isDark) {
+        localStorage.setItem('darkMode', 'dark');
+    } else {
+        localStorage.setItem('darkMode', 'light');
+    }
+}
